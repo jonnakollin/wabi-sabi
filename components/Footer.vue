c0ae9bb236d29a4e24901b86664050e0ea62db14
jonnakollin <joonnaaa_@hotmail.com>
2019-03-16 19:31:03 +0100

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">© 2019 JONNA KOLLIN | WABI-SABI</footer>
+</template>
+
+<style lang="less">
+.footer {
+  background-color: #dbdad8;
+  text-align: center;
+  padding: 40px 0;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,22 @@
 <template>
   <div>
     <Header/>
-    <main class="main">
-      <div class="wrapper">
-        <nuxt/>
-      </div>
-    </main>
+    <nuxt/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header.vue";
+import Footer from "~/components/Footer.vue";
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
 
 <style lang="less" scoped>
-.main {
-  padding: 20px;
-  @media only screen and (min-width: 768px) {
-    padding: 60px 20px;
-  }
-}
-
-.wrapper {
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 1080px;
-}
 </style>
