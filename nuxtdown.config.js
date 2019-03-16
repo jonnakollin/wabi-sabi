0fe702fcb901b732e99cafedfd8df66a5c04c547
jonnakollin <joonnaaa_@hotmail.com>
2019-03-16 23:13:22 +0100

    url fix

--- nuxtdown.config.js
+++ nuxtdown.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   api: function (isStatic) {
     const baseURL = 'http://localhost:3000';
-    const browserBaseURL = !isStatic ? "" : 'https://wabi-sabi.netlify.com';
+    const browserBaseURL = !isStatic ? "" : process.env.BASE_URL;
 
     return {
       baseURL,
